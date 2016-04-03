ce2b67dbc11220aa9cab1da0ce9d20e09343158e
dance2die <dontbothermewithspam@gmail.com>
2016-04-03 11:09:15 -0400

    Downloaded weather data

--- app/src/main/java/com/dance2die/demoandroidudemyjsondemo/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemyjsondemo/MainActivity.java
@@ -1,15 +1,58 @@
 package com.dance2die.demoandroidudemyjsondemo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+                while (data != -1){
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.i("Website Content", result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +69,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DownloadTask task = new DownloadTask();
+        // Content is displayed in LogCat via "onPostExecute" method.
+        task.execute("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=286c90a2b702c059135ae9f427fbc5ac");
     }
 
     @Override
